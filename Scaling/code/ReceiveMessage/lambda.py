0ef701344a3287fa931ee9fb836ad7b35632091f
Przemek Malak <przemek.malak@gmail.com>
2019-04-29 08:36:20 +0200

    Delete fix

--- Scaling/code/ReceiveMessage/lambda.py
+++ Scaling/code/ReceiveMessage/lambda.py
@@ -10,7 +10,9 @@ def lambda_handler(event, context):
     handle = event['Records'][0]['receiptHandle']
     time.sleep(0.5)
     print('Time: ' + str(datetime.datetime.now()) + ' Id: ' + uuid)
+    '''
     response = client.delete_message(
         QueueUrl='https://sqs.eu-west-1.amazonaws.com/655379451354/4developers_queue',
         ReceiptHandle=handle
     )
+    '''
